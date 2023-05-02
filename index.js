414db3a56c823a041a5ea26ee4c91f3472c9dfc0
Melissa Sanchez <melissasanchez064@gmail.com>
2023-05-02 14:55:58 -0700

    Complete Lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = (num1 * num2);
+
+const random = Math.floor(Math.random()*10) +1;
+
+let num3 = 16;
+let num4 = 4;
+let mod = 16/4;
+
+let max = 20;
